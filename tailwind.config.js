6559f78fcd43d5127819f814e1f4f69bb52cf38e
Sergiopata <sergiospatacino@gmail.com>
2022-08-02 00:56:12 -0300

    Agregando primera sección

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/*/.{html,js}',
+  './index.html',],
+  theme: {
+    fontFamily: {
+      'ibarra': ['Ibarra Real Nova', 'serif'],
+      'roboto': ['Roboto', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
